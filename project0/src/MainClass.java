f7f628578d2fb9abd8ca2fc726cddbb66fc4dcf5
leemirim <leemirim9654@gmail.com>
2019-05-06 17:20:53 +0900

    변수 개념 학습

--- project0/src/MainClass.java
+++ project0/src/MainClass.java
@@ -1,5 +1,8 @@
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Hello Java World!!!!!!!!!!");
+        int i = 10;
+        int j = 100;
+
+        System.out.println("i + j = " + (i+j));
     }
 }
